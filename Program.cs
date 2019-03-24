f0aebe3abba86d9acaf87480ad2d97c7dbbf1e48
Tuğçe <39226858+TugceBuyukdeveci@users.noreply.github.com>
2019-03-24 18:30:46 +0200

    ilk gönderi

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aklımdaki_sayı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int tut = r.Next(100);
+            while (true)
+            {
+                Console.WriteLine("Tuttuğum Sayıyı Bulabilir misin?:");
+                int gs = Convert.ToInt32(Console.ReadLine());
+                if (gs > tut) Console.WriteLine("büyük sayı girdin.");
+                else if (gs < tut) Console.WriteLine("küçük sayı girdin.");
+                else
+                {
+                    Console.WriteLine("harikasın.BİNNGGGGOOOOOOOO"); break;
+                }
+            }
+        }
+    }
+}
